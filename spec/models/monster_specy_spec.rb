adc394ddd19e84850abfe4724b3d08bde654e904
yukimura1227 <takamura1227@gmail.com>
2017-09-17 14:01:55 +0000

    add association spec

--- spec/models/monster_specy_spec.rb
+++ spec/models/monster_specy_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe MonsterSpecy, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { is_expected.to belong_to(:monster) }
+    it { is_expected.to belong_to(:species) }
+  end
 end
