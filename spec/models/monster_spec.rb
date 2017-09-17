e021200a0ff085025b02c3ac8e871d20a55984a4
yukimura1227 <takamura1227@gmail.com>
2017-09-17 13:52:38 +0000

    check Attribute class name on belong_to association

--- spec/models/monster_spec.rb
+++ spec/models/monster_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Monster, type: :model do
   describe 'Associations' do
     it { is_expected.to belong_to(:skill) }
-    it { is_expected.to belong_to(:element) }
-    it { is_expected.to belong_to(:sub_element) }
+    it { is_expected.to belong_to(:element).class_name('Attribute') }
+    it { is_expected.to belong_to(:sub_element).class_name('Attribute') }
   end
 end
