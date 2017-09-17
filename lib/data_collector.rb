90f764837d7e055bf43042df4be4b830a2e82e62
yukimura1227 <takamura1227@gmail.com>
2017-09-17 15:38:05 +0000

    refactor

--- lib/data_collector.rb
+++ lib/data_collector.rb
@@ -2,13 +2,8 @@ require 'nokogiri'
 require 'open-uri'
 
 class DataCollector
-  def self.sample
-    url = 'http://pd.appbank.net/ml1'
-    charset = nil
-    html = open(url) do |f|
-      charset = f.charset
-      f.read
-    end
+  def self.sample(url = 'http://pd.appbank.net/ml1')
+    html, charset = extract_html_and_charset(url)
 
     doc = Nokogiri::HTML.parse(html, nil, charset)
     doc.xpath("//ul[contains(@class,'list-round-rect')]").each do |node|
@@ -16,5 +11,21 @@ class DataCollector
         puts "遷移先： #{a_tag_node[:href]} リンク名: #{a_tag_node.inner_html}"
       end
     end
+    doc.xpath("//ul[contains(@class,'list-box')]").each do |node|
+      node.xpath('li//a').each do |a_tag_node|
+        puts "遷移先： #{a_tag_node[:href]} リンク名: #{a_tag_node.inner_html}"
+      end
+    end
+  end
+
+  private
+
+  def self.extract_html_and_charset(url)
+    charset = nil
+    html = open(url) do |f|
+      charset = f.charset
+      f.read
+    end
+    [html, charset]
   end
 end
