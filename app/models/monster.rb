c3549f18cfbb22142b62feaeb493f65a2d29f86e
yukimura1227 <takamura1227@gmail.com>
2017-09-17 13:43:13 +0000

    add association for element and sub element

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,3 +1,7 @@
 class Monster < ApplicationRecord
   belongs_to :skill
+  with_options class_name: 'Attribute' do
+    belongs_to :element, foreign_key: :element_id
+    belongs_to :sub_element, foreign_key: :sub_element_id
+  end
 end
