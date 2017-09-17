06f5bfe975622d9bad5a3ccf7c48efe04f66fcb9
yukimura1227 <takamura1227@gmail.com>
2017-09-17 07:45:44 +0000

    add schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170917061847) do
+ActiveRecord::Schema.define(version: 20170917064325) do
+
+  create_table "attributes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -25,6 +31,41 @@ ActiveRecord::Schema.define(version: 20170917061847) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["blog_id"], name: "index_comments_on_blog_id"
+  end
+
+  create_table "monster_species", force: :cascade do |t|
+    t.integer "monster_id_id"
+    t.integer "species_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["monster_id_id"], name: "index_monster_species_on_monster_id_id"
+    t.index ["species_id"], name: "index_monster_species_on_species_id"
+  end
+
+  create_table "monsters", force: :cascade do |t|
+    t.integer "uid", null: false
+    t.integer "attribute_id", null: false
+    t.integer "sub_attribute_id"
+    t.integer "monster_species_id", null: false
+    t.integer "skill_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["skill_id"], name: "index_monsters_on_skill_id"
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "explain"
+    t.integer "default_turn"
+    t.integer "minimum_turn"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "species", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
