244224b2a9e9c254ed800e7d35ce169cf12b3faf
yukimura1227 <takamura1227@gmail.com>
2017-09-17 14:09:19 +0000

    implements monster has many species

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -4,4 +4,6 @@ class Monster < ApplicationRecord
     belongs_to :element, foreign_key: :element_id
     belongs_to :sub_element, foreign_key: :sub_element_id
   end
+  has_many :monster_species
+  has_many :species, through: :monster_species
 end
--- spec/models/monster_spec.rb
+++ spec/models/monster_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe Monster, type: :model do
     it { is_expected.to belong_to(:skill) }
     it { is_expected.to belong_to(:element).class_name('Attribute') }
     it { is_expected.to belong_to(:sub_element).class_name('Attribute') }
+    it { is_expected.to have_many(:monster_species) }
+    it { is_expected.to have_many(:species).through(:monster_species) }
   end
 end
