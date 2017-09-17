e43eadf0e5e8ddf337c78231d1d1daaeaeeac789
yukimura1227 <takamura1227@gmail.com>
2017-09-17 11:27:40 +0000

    refactor

--- spec/models/attribute_spec.rb
+++ spec/models/attribute_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Attribute, type: :model do
-  describe 'recrods size' do
-    it 'is 5(火,水,木,光,闇)' do
-      expect(Attribute.pluck(:name)).to contain_exactly('火', '水', '木', '光', '闇')
+  describe 'Attribute records' do
+    it do
+      expect(described_class.pluck(:name)).to contain_exactly(
+        '火', '水', '木', '光', '闇'
+      )
     end
   end
 end
