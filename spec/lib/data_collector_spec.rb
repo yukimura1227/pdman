b958dd0b33723301f44d9b2caba5a1841e753bcf
yukimura1227 <takamura1227@gmail.com>
2017-09-17 23:55:45 +0000

    add spec

--- spec/lib/data_collector_spec.rb
+++ spec/lib/data_collector_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe DataCollector do
+  describe '.extract_html_and_charset' do
+    it 'can store monsters' do
+      DataCollector.sample
+      expect(Monster.find(10).name).to eq 'ブラッキオ'
+      expect(Monster.count).to eq 100
+    end
+  end
 end
