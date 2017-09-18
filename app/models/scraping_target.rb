44032d40ea0f744512eaaaeb7b717d131eeafae9
yukimura1227 <takamura1227@gmail.com>
2017-09-18 11:31:32 +0000

    implements monster_list_page collect function on scraping_target/monster_list_page.rb

--- app/models/scraping_target.rb
+++ app/models/scraping_target.rb
@@ -1,2 +1,15 @@
+require 'nokogiri'
+require 'open-uri'
+
 class ScrapingTarget < ApplicationRecord
+  private
+
+  def extract_html_and_charset(url)
+    charset = nil
+    html = open(url) do |f|
+      charset = f.charset
+      f.read
+    end
+    [html, charset]
+  end
 end
--- app/models/scraping_target/monster_list_page.rb
+++ app/models/scraping_target/monster_list_page.rb
@@ -1,4 +1,22 @@
 class ScrapingTarget
   class MonsterListPage < ScrapingTarget
+    def scraping
+      html, charset = extract_html_and_charset('http://pd.appbank.net' + url)
+      doc = Nokogiri::HTML.parse(html, nil, charset)
+      extract_monster_list_pages(doc)
+    end
+
+    private
+
+    def extract_monster_list_pages(doc)
+      doc.xpath("//ul[contains(@class,'list-round-rect')]").each do |node|
+        node.xpath('li//a').each do |a_tag_node|
+          page = ScrapingTarget::MonsterListPage.where(
+            url: a_tag_node[:href]
+          ).first_or_create
+          page.update(link_name: a_tag_node.inner_html)
+        end
+      end
+    end
   end
 end
