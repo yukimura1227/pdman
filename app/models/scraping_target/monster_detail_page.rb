aa159932958cdffdc7322c841a298bb629d72ea5
yukimura1227 <takamura1227@gmail.com>
2017-09-19 01:06:40 +0000

    get monster detail page html

--- app/models/scraping_target/monster_detail_page.rb
+++ app/models/scraping_target/monster_detail_page.rb
@@ -1,4 +1,10 @@
 class ScrapingTarget
+  # モンスター詳細ページの解析を担う
   class MonsterDetailPage < ScrapingTarget
+    def scraping
+      html, charset = extract_html_and_charset(extract_target_url)
+      doc = Nokogiri::HTML.parse(html, nil, charset)
+      puts doc.to_html
+    end
   end
 end
