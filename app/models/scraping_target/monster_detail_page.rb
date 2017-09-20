11ce9d3201c32f87cfc14dbc29998b791ae08333
yukimura1227 <takamura1227@gmail.com>
2017-09-20 01:55:41 +0000

    extract monster types(sample)

--- app/models/scraping_target/monster_detail_page.rb
+++ app/models/scraping_target/monster_detail_page.rb
@@ -4,7 +4,21 @@ class ScrapingTarget
     def scraping
       html, charset = extract_html_and_charset(extract_target_url)
       doc = Nokogiri::HTML.parse(html, nil, charset)
-      puts doc.to_html
+      # puts doc.to_html
+      extract_monster_detail_page(doc)
+    end
+
+    private
+
+    def extract_monster_detail_page(doc)
+      monster_div = doc.xpath("//div[contains(@class, 'monster')]").first
+      monster_types_area_div =
+        monster_div.xpath("div[contains(@class, 'spacer')]").first
+      monster_types_tag =
+        monster_types_area_div.xpath("p[contains(@class, 'icon-mtype')]").first
+      monster_types_tag.xpath('a').each do |node|
+        puts node.inner_html
+      end
     end
   end
 end
