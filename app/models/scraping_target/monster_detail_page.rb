c4a57f081a72750148d630a73809b423e8a994ab
yukimura1227 <takamura1227@gmail.com>
2017-09-20 14:54:35 +0000

    register monster(attribute only uid and name)

--- app/models/scraping_target/monster_detail_page.rb
+++ app/models/scraping_target/monster_detail_page.rb
@@ -14,11 +14,30 @@ class ScrapingTarget
       monster_div = doc.xpath("//div[contains(@class, 'monster')]").first
       monster_types_area_div =
         monster_div.xpath("div[contains(@class, 'spacer')]").first
+      uid, name = extract_moster_id_an_name(monster_div)
+
       monster_types_tag =
         monster_types_area_div.xpath("p[contains(@class, 'icon-mtype')]").first
       monster_types_tag.xpath('a').each do |node|
         puts node.inner_html
       end
+      update_monster(uid, name)
+    end
+
+    def extract_moster_id_an_name(monster_div)
+      target =
+        monster_div.xpath("h2[contains(@class, 'title-bg')]").first.inner_html
+      /No\.(?<capture_uid>\d+)\s(?<capture_name>.+)/ =~ target
+      [capture_uid, capture_name]
+    end
+
+    def update_monster(uid, name)
+      m = Monster.find_or_initialize_by(uid: uid)
+      Skill.first_or_create!(id: 1, name: :hoge) # TODO: dummy
+      m.update!(
+        uid: uid, name: name,
+        skill_id: 1, element_id: 1, sub_element_id: 2, monster_species_id: 1
+      ) # TODO: element and species is dummy!!!!
     end
   end
 end
