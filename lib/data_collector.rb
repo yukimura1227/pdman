3bbd9d524efd53235e18fa804439d6ced0346075
yukimura1227 <takamura1227@gmail.com>
2017-09-21 01:13:54 +0000

    use mock for saving scraping target

--- lib/data_collector.rb
+++ lib/data_collector.rb
@@ -1,9 +1,20 @@
+# パズドラのモンスター情報を収集する
 class DataCollector
-  def self.sample(default_url = '/ml1')
+  def sample(default_url = '/ml1')
     if ScrapingTarget::MonsterListPage.count.zero?
       ScrapingTarget::MonsterListPage.where(url: default_url).first_or_create
     end
-    ScrapingTarget::MonsterListPage.all.each(&:scraping)
-    ScrapingTarget::MonsterDetailPage.all.each(&:scraping)
+    all_monster_list_page.each(&:scraping)
+    all_monster_detail_page.each(&:scraping)
+  end
+
+  private
+
+  def all_monster_list_page
+    ScrapingTarget::MonsterListPage.all
+  end
+
+  def all_monster_detail_page
+    ScrapingTarget::MonsterDetailPage.all
   end
 end
--- spec/lib/data_collector_spec.rb
+++ spec/lib/data_collector_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe DataCollector do
-  describe '.extract_html_and_charset' do
+  describe '#sample' do
+    let(:collector) { DataCollector.new }
+    before do
+      # テストで全件スクレイピングしたくないので、制御するようメソッドをモック化
+      allow(collector).to(
+        receive(:all_monster_list_page)
+          .and_return(ScrapingTarget::MonsterListPage.limit(2))
+      )
+      allow(collector).to(
+        receive(:all_monster_detail_page)
+          .and_return(ScrapingTarget::MonsterDetailPage.limit(2))
+      )
+    end
     it 'can store monsters' do
-      DataCollector.sample
+      collector.sample
       expect(Monster.find(10).name).to eq 'ブラッキオ'
       expect(Monster.count).to eq 100
       expect(ScrapingTarget::MonsterListPage.count).to eq 40
