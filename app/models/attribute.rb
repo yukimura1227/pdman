c630bff9358a6c7920414c494ff070c6b1cf6830
yukimura1227 <takamura1227@gmail.com>
2017-09-23 13:53:54 +0000

    add association attributes has many monsters(related by sub_element_id)

--- app/models/attribute.rb
+++ app/models/attribute.rb
@@ -1,4 +1,7 @@
 # 属性を表すモデル
 class Attribute < ApplicationRecord
-  has_many :monsters, foreign_key: :element_id
+  with_options class_name: 'Monster' do
+    has_many :monsters, foreign_key: :element_id
+    has_many :monsters_by_sub_attribute, foreign_key: :sub_element_id
+  end
 end
--- spec/models/attribute_spec.rb
+++ spec/models/attribute_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Attribute, type: :model do
   describe 'Associations' do
     it { is_expected.to have_many(:monsters) }
+    it { is_expected.to have_many(:monsters_by_sub_attribute) }
   end
   describe 'Attribute records' do
     it do
