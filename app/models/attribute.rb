42141b953720aec26e6293dd16ab4f8ddf10133b
yukimura1227 <takamura1227@gmail.com>
2017-09-23 13:50:01 +0000

    add association attributes has many monsters

--- app/models/attribute.rb
+++ app/models/attribute.rb
@@ -1,3 +1,4 @@
 # 属性を表すモデル
 class Attribute < ApplicationRecord
+  has_many :monsters, foreign_key: :element_id
 end
--- spec/models/attribute_spec.rb
+++ spec/models/attribute_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Attribute, type: :model do
+  describe 'Associations' do
+    it { is_expected.to have_many(:monsters) }
+  end
   describe 'Attribute records' do
     it do
       expect(described_class.pluck(:name)).to contain_exactly(
