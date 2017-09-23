723dcf84fd86e496d52a38f37e370100f44a5f4b
yukimura1227 <takamura1227@gmail.com>
2017-09-23 12:47:49 +0000

    fix lint

--- app/models/scraping_target/monster_detail_page.rb
+++ app/models/scraping_target/monster_detail_page.rb
@@ -5,8 +5,7 @@ class ScrapingTarget
       return if last_scraping_at.present? && last_scraping_at > 1.day.ago
       sleep 1
       update(last_scraping_at: Time.zone.now, is_success_last: false)
-      html, charset = extract_html_and_charset(extract_target_url)
-      doc = Nokogiri::HTML.parse(html, nil, charset)
+      doc = parse_target
       extract_monster_detail_page(doc)
       update(is_success_last: true)
     end
